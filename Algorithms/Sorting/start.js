29d62c65530e1db792b1fb25071f4977a5d58238
Rumanzi <tpatrickrumanzi@gmail.com>
2026-01-18 01:02:18 +0000

    sorting

---
+++ Algorithms/Sorting/start.js
@@ -0,0 +1,23 @@
+// arr=[1,3,4,67,87,23,68,8]
+
+//write a function that would sort an array in ascending order
+
+function sortAsc(arr){
+let sorted=arr.sort(function(a,b){
+    return a-b
+})
+return sorted
+    
+}
+//testing
+// console.log(sortAsc([1,3,4,67,87,23,68,8]))
+
+//write a function that would sort an array in descending order
+
+function sortDesc(arr){
+    let sorted=arr.sort(function(a,b){
+        return b-a
+    })
+    return sorted
+}
+// console.log(sortDesc([1,3,4,67,87,23,68,8]))
